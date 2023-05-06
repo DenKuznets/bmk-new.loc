96d7a0b8e622e96b6cf652edef9e464e00796ea0
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-05-06 12:35:34 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     },
     build: {
         outDir: "./public_html/build/",
-        cssSourceMap: true,
+        cssSourceMap: true, //это тоже показывать из какого файла взят css , но заработало после добавления верхнего :)
     },
     plugins: [
         laravel({
